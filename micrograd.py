fbca4f72f584901532b438760498d2da85de6de0
mckinlde <douglas.e.mckinley@gmail.com>
2025-06-02 11:33:02 -0700

    git init

---
+++ micrograd.py
@@ -0,0 +1,10 @@
+# following along with Andrej's Jyupter Notebook
+
+# micrograd is an automatic gradient engine, it implements backpropogation
+
+# backpropogation tunes weights to iteratively tune the loss function
+
+# micrograd allows you to build out mathematical expressions
+
+# gradient is a fancy word for the derivative of F(x) with respect to G(x)
+
